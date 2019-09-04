c2186af7496cc6a9b088a03681706096433370dd
Perera <vindya.perera@pearson.com>
2019-09-04 14:22:48 +0530

    dev branch creation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,31 @@
 package com.example.demo;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableOAuth2Sso
 @SpringBootApplication
 @EnableSwagger2
+@RestController
+
+
+
 public class DemoApplication {
+	
+	@RequestMapping("/user")
+	  public Principal user(Principal principal) {
+	    return principal;
+	  }
 
 	public static void main(String[] args) {
+	
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
